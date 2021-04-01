6940cf2d616b97f3c98207be139842b192e0a514
Oleksandr Pomalin <nilamopr@gmail.com>
2021-04-01 12:56:13 +0300

    add HomePage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import * as getFetch from '../servises/tmdb-api';
+import TrendingList from '../components/TrendingList/TrendingList';
+import { render } from '@testing-library/react';
+
+class HomePage extends Component {
+  render() {
+    return <> </>;
+  }
+}
+
+export default HomePage;
