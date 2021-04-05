0cc77af26192fd0d82d1cd00e8ee252d5115712f
Oleksandr Pomalin <nilamopr@gmail.com>
2021-04-05 03:06:13 +0300

    adds routing, def exp, lazy loading

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import { lazy } from 'react';
+import { v4 as uuid } from 'uuid';
+
+const routes = [
+  {
+    key: uuid(),
+    exact: true,
+    path: '/',
+    component: lazy(() => import('./pages/HomePage')),
+  },
+  {
+    key: uuid(),
+    exact: true,
+    path: '/movies',
+    component: lazy(() => import('./pages/MoviesPage')),
+  },
+  {
+    key: uuid(),
+    exact: true,
+    path: '/about',
+    component: lazy(() => import('./pages/About')),
+  },
+];
+export default routes;
